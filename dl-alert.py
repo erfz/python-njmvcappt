518f273afa49482e4a945f0a715de3c1a4c0ebe9
Jason Guo <jsg353@cornell.edu>
2021-07-12 00:41:28 -0400

    Formatting

--- dl-alert.py
+++ dl-alert.py
@@ -5,10 +5,47 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import winsound
 
-location_arr = ['186', '189', '208', '191', '192', '195', '197', '203', '204', '207', '188', '190', '193', '196', '199', '202', '205']
-locationname_arr = ['Bakers Basin','Camden','Cardiff','Delanco','Eatontown','Flemington','Freehold','Oakland','Paterson','Randolph','Rio Grande','Salem', 'South Plainfield', 'Toms River', 'Vineland', 'Wayne', 'West Deptford']
-base_url_link='https://telegov.njportal.com/njmvc/AppointmentWizard/15/'
-required_months = ['July','August']
+location_arr = [
+    "186",
+    "189",
+    "208",
+    "191",
+    "192",
+    "195",
+    "197",
+    "203",
+    "204",
+    "207",
+    "188",
+    "190",
+    "193",
+    "196",
+    "199",
+    "202",
+    "205",
+]
+locationname_arr = [
+    "Bakers Basin",
+    "Camden",
+    "Cardiff",
+    "Delanco",
+    "Eatontown",
+    "Flemington",
+    "Freehold",
+    "Oakland",
+    "Paterson",
+    "Randolph",
+    "Rio Grande",
+    "Salem",
+    "South Plainfield",
+    "Toms River",
+    "Vineland",
+    "Wayne",
+    "West Deptford",
+]
+base_url_link = "https://telegov.njportal.com/njmvc/AppointmentWizard/15/"
+required_months = ["July", "August"]
+
 
 def beep():
     winsound.Beep(1500, 500)
@@ -23,33 +60,40 @@ def job():
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
     print("\n\n\nDate Time: ", dt_string, "\n\n")
-    i=0
-    found=0
-    
-    
+    i = 0
+    found = 0
+
     for location in location_arr:
         print(location)
-        with urllib.request.urlopen(base_url_link+location) as response:
+        with urllib.request.urlopen(base_url_link + location) as response:
             page_html = response.read()
-        soup = BeautifulSoup(page_html ,'lxml')
-        unavailable=soup.find('div',attrs={'class': 'alert-danger'})
-        if unavailable is not None :
-            #print('No appointments are available in '+locationname_arr[i])
-            dt_string=""
+        soup = BeautifulSoup(page_html, "lxml")
+        unavailable = soup.find("div", attrs={"class": "alert-danger"})
+        if unavailable is not None:
+            # print('No appointments are available in '+locationname_arr[i])
+            dt_string = ""
         else:
-            dates_html = soup.find('div',attrs={'class': 'col-md-8'})
-            date_string = dates_html.find('label',attrs={'class': 'control-label'})
+            dates_html = soup.find("div", attrs={"class": "col-md-8"})
+            date_string = dates_html.find("label", attrs={"class": "control-label"})
             if set(required_months) & set(date_string.text.split()):
-                #print("Matching required months")
-                date_string=re.sub('Time of Appointment for ', '', date_string.text)
-                date_string=re.sub(':', '', date_string)
-                message = 'DL Renew Dates: '+locationname_arr[i]+' / ('+location+') : '+date_string
+                # print("Matching required months")
+                date_string = re.sub("Time of Appointment for ", "", date_string.text)
+                date_string = re.sub(":", "", date_string)
+                message = (
+                    "DL Renew Dates: "
+                    + locationname_arr[i]
+                    + " / ("
+                    + location
+                    + ") : "
+                    + date_string
+                )
                 print(message)
                 beep()
-                found=1
-        i=i+1
-        
-while True :
+                found = 1
+        i = i + 1
+
+
+while True:
     try:
         job()
     except:
@@ -57,4 +101,3 @@ while True :
         time.sleep(60)
     else:
         time.sleep(60)
-    
